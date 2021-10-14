154f6c0544425db85a8f1fa6de827607ee375a33
XXXTENTACION <88360151+xxxtention-TG@users.noreply.github.com>
2021-10-14 11:32:27 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -13,35 +13,28 @@ from .Channel import (
 HELP = """
 <b><u>Basic Commads</u></b>
 
-➥ /start : check if am alive or dead
-➥ /about : About Me
-➥ /stats user Status
+◯ /start : check if am alive or dead
+◯ /about : About Me
+◯ /stats user Status
 
 <b><u>Bot Owner Only</u></b>
 
-➥ /broadcast Replay Any Message or Media
-➥ /ban_user  click ban_user more info
-➥ /unban_user click unban_user more info
-➥ /banned_users Banned User Details
-➥ /total How Many Files Added In Database
-➥ /logger  Get Logs
-➥ /delete Delete File From Database
+◯ /broadcast Replay Any Message or Media
+◯ /ban_user  click ban_user more info
+◯ /unban_user click unban_user more info
+◯ /banned_users Banned User Details
+◯ /total How Many Files Added In Database
+◯ /logger  Get Logs
+◯ /delete Delete File From Database
 
-<b>[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT></b>
+<b>[𝖬𝗈𝗏𝗂𝖾𝗌 𝖧𝗎𝖻](https://t.me/movieshub_group)</b>
 """
 
 ABOUT = """
-➥ Name : Auto Filter With IMDB
-    
-➥ Credits : Subin
-
-➥ Editing : [Muhammed RK](t.me/Mrk_YT)
-
-➥ Language : Python3
-
-➥ Library : Pyrogram Asyncio 1.13.0
-
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ ɴᴀᴍᴇ : ᴅᴇᴀᴅᴘᴏᴏʟ
+➥ ᴄʀᴇᴀᴛᴏʀ : [ᴅᴀʀᴋ ᴀɴɢᴇʟ](https://t.me/darkz_angel)
+➥ ᴇᴅɪᴛᴏʀ : [ᴇʟᴏɴ ᴍᴜsᴋ](t.me/elon_musk3)
+➥ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ3
+➥ ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ ᴀsʏɴᴄɪᴏ 1.13.0
+◯ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : 🔐
 """
